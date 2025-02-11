28f25f16a6abdd45f3e10a05acecebe80301882a
TesTed45 <jamesvictoralvarez@gmail.com>
2025-02-11 12:21:19 -0500

    Added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Hero from "@/components/Hero";
+import Menu from "@/components/Menu";
 
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="bg-black">
+      
       <div className="">
+        <Menu />
         <Hero />
       </div>
     </main>
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,128 +1,39 @@
-// import React, { useState, useEffect } from "react";
-// import {
-//   Navbar,
-//   MobileNav,
-//   Typography,
-//   Button,
-//   Menu,
-//   MenuHandler,
-//   MenuList,
-//   MenuItem,
-//   Avatar,
-//   Card,
-//   IconButton,
-// } from "@material-tailwind/react";
-// import {
-//   CubeTransparentIcon,
-//   UserCircleIcon,
-//   CodeBracketSquareIcon,
-//   Square3Stack3DIcon,
-//   ChevronDownIcon,
-//   Cog6ToothIcon,
-//   InboxArrowDownIcon,
-//   LifebuoyIcon,
-//   PowerIcon,
-//   RocketLaunchIcon,
-//   Bars2Icon,
-// } from "@heroicons/react/24/solid";
+import React from 'react'
 
-// const profileMenuItems = [
-//   { label: "My Profile", icon: UserCircleIcon },
-//   { label: "Edit Profile", icon: Cog6ToothIcon },
-//   { label: "Inbox", icon: InboxArrowDownIcon },
-//   { label: "Help", icon: LifebuoyIcon },
-//   { label: "Sign Out", icon: PowerIcon },
-// ];
+function Menu() {
+  return (
+    <nav className="block w-full max-w-screen-lg px-4 py-2 mx-auto bg-black-500 bg-opacity-90 sticky top-3 shadow rounded-xl lg:px-8 lg:py-3 backdrop-blur-lg backdrop-saturate-150 z-[9999] mt-10">
+    <div className="container flex flex-wrap items-center justify-between mx-auto text-white">
+        <a href="#"
+        className="mr-4 block cursor-pointer py-1.5 text-base text-white font-semibold">
+            James Victor Alvarez
+        </a>
+        <div className="hidden lg:block">
+        <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+            <li className="flex items-center p-1 text-sm gap-x-2 text-white">
+            <a href="#" className="flex items-center">Pages</a>
+            </li>
+            <li className="flex items-center p-1 text-sm gap-x-2 text-white">
+            <a href="#" className="flex items-center">Account</a>
+            </li>
+            <li className="flex items-center p-1 text-sm gap-x-2 text-white">
+            <a href="#" className="flex items-center">Blocks</a>
+            </li>
+            <li className="flex items-center p-1 text-sm gap-x-2 text-white">
+            <a href="#" className="flex items-center">Docs</a>
+            </li>
+        </ul>
+        </div>
+        <button
+        className="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden"
+        type="button">
+        <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
 
-// function ProfileMenu() {
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-//   const closeMenu = () => setIsMenuOpen(false);
+        </span>
+        </button>
+    </div>
+    </nav>
+  )
+}
 
-//   return (
-//     <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
-//       <MenuHandler>
-//         <Button variant="text" color="blue-gray" className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto">
-//           <Avatar
-//             variant="circular"
-//             size="sm"
-//             alt="Profile"
-//             className="border border-gray-900 p-0.5"
-//             src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?auto=format&fit=crop&w=1480&q=80"
-//           />
-//           <ChevronDownIcon strokeWidth={2.5} className={`h-3 w-3 transition-transform ${isMenuOpen ? "rotate-180" : ""}`} />
-//         </Button>
-//       </MenuHandler>
-//       <MenuList className="p-1">
-//         {profileMenuItems.map(({ label, icon }, key) => {
-//           const isLastItem = key === profileMenuItems.length - 1;
-//           return (
-//             <MenuItem
-//               key={label}
-//               onClick={closeMenu}
-//               className={`flex items-center gap-2 rounded ${isLastItem ? "hover:bg-red-500/10" : ""}`}
-//             >
-//               {React.createElement(icon, { className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}` })}
-//               <Typography as="span" variant="small" className="font-normal" color={isLastItem ? "red" : "inherit"}>
-//                 {label}
-//               </Typography>
-//             </MenuItem>
-//           );
-//         })}
-//       </MenuList>
-//     </Menu>
-//   );
-// }
-
-// const navListItems = [
-//   { label: "Account", icon: UserCircleIcon },
-//   { label: "Blocks", icon: CubeTransparentIcon },
-//   { label: "Docs", icon: CodeBracketSquareIcon },
-// ];
-
-// function NavList() {
-//   return (
-//     <ul className="flex flex-col gap-2 lg:flex-row lg:items-center">
-//       {navListItems.map(({ label, icon }) => (
-//         <Typography key={label} as="a" href="#" variant="small" color="gray" className="font-medium text-blue-gray-500">
-//           <MenuItem className="flex items-center gap-2 lg:rounded-full">
-//             {React.createElement(icon, { className: "h-[18px] w-[18px]" })} {label}
-//           </MenuItem>
-//         </Typography>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// const ComplexNavbar = () => {
-//   const [isNavOpen, setIsNavOpen] = useState(false);
-//   const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
-
-//   useEffect(() => {
-//     window.addEventListener("resize", () => window.innerWidth >= 960 && setIsNavOpen(false));
-//   }, []);
-
-//   return (
-//     <Navbar className="mx-auto max-w-screen-xl p-2 lg:rounded-full lg:pl-6">
-//       <div className="flex items-center justify-between text-blue-gray-900">
-//         <Typography as="a" href="#" className="mr-4 ml-2 cursor-pointer py-1.5 font-medium">
-//           Material Tailwind
-//         </Typography>
-//         <div className="hidden lg:block">
-//           <NavList />
-//         </div>
-//         <IconButton size="sm" color="blue-gray" variant="text" onClick={toggleIsNavOpen} className="ml-auto mr-2 lg:hidden">
-//           <Bars2Icon className="h-6 w-6" />
-//         </IconButton>
-//         <Button size="sm" variant="text">
-//           <span>Log In</span>
-//         </Button>
-//         <ProfileMenu />
-//       </div>
-//       <MobileNav open={isNavOpen} className="overflow-scroll">
-//         <NavList />
-//       </MobileNav>
-//     </Navbar>
-//   );
-// };
-
-// export default ComplexNavbar;
+export default Menu
